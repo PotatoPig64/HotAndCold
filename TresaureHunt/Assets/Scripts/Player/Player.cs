bee9bdd3b520ee8ce55e195871cc9acf70e61b4c
PotatoPig64 <74412624+PotatoPig64@users.noreply.github.com>
2021-01-22 14:59:00 +0100

    Player Script + 2d extras master
    laddat ner 2d extras master till projecktet så vi kan göra allt med grafiken, har även lagt in lite tillfälliga tiles jag hade som låg och drällde på datorn som man kan använda nu ibörjan om man behöver.
    Spelarens script är även med här, men det är långt ifrån klart, jag vill bara lägga upp allt på main branchen nu innan jag glömmer.

---
+++ TresaureHunt/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //misc
+    public bool isUnderWater;
+    private Rigidbody2D rb;
+    private BoxCollider2D boxColl;
+    public Collider2D meleeAttackTrigger;
+    public Animator animator;
+    public float moveSpeed;
+
+    //ground check
+    private bool isGrounded;
+    public Transform groundCheck;
+    public LayerMask whatIsGround;
+    public float checkRadius;
+
+    //Jump
+    private float jumptimeCounter;
+    public float jumpTime;
+    public float fallMultiplyer;
+    public bool isJumping;
+
+    //melee attack
+    public bool isAttacking;
+    private float meeleAttackTimer;
+    public float meleeAttackCooldown;
+
+    //rangeAttack
+    public Transform bullet;
+    public float bulletSpeed;
+    public float coolDownRangeAttack;
+    private float coolDownTimerRangeAttack;
+
+    //breathing mechanics
+    private float airMax;
+    public float airCountdown;
+    public bool isBreathing;
+    public bool isDrowning;
+
+
+    void Start()
+    {
+        rb.GetComponent<Rigidbody2D>(); //enables access to the player's rigidbody
+        boxColl.GetComponent<BoxCollider2D>();// enables access to the player's boxcollider
+    }
+
+    private void FixedUpdate()
+    {
+        //checks if the player is on the ground or not
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+        //movement
+        if (Input.GetKey(KeyCode.D)) { transform.position = new Vector3(transform.position.x + moveSpeed * Time.deltaTime, transform.position.y); }
+        if (Input.GetKey(KeyCode.A)) { transform.position = new Vector3(transform.position.x - moveSpeed * Time.deltaTime, transform.position.y); }
+        if (Input.GetKey(KeyCode.W)) { transform.position = new Vector3(transform.position.x, transform.position.y + moveSpeed * Time.deltaTime, transform.position.z + moveSpeed * Time.deltaTime); }
+        if (Input.GetKey(KeyCode.S)) { transform.position = new Vector3(transform.position.x, transform.position.y - moveSpeed * Time.deltaTime, transform.position.z - moveSpeed * Time.deltaTime); }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
