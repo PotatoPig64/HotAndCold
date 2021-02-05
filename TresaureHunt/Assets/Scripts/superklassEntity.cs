e4b2e4df68bdeb788369e9b1500186c73faa2126
beeb7 <74414637+beeb7@users.noreply.github.com>
2021-02-05 14:21:31 +0100

    superklass 1.5

---
+++ TresaureHunt/Assets/Scripts/superklassEntity.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class superklassEntity : MonoBehaviour
+{
+    
+        void Movement()
+        {
+
+
+
+
+        }
+         
+        void Attack()
+        {
+
+
+
+        }
+
+        void Health()
+        {
+            float Health = 1;
+
+            if (Health <= 0.1f)
+            {
+                Health = 1f;
+            }
+
+            /*is used when the player runns out of lives
+            if (Health == 0)
+            {
+            managerScript.gameOver = true;
+            }*/
+
+    }
+
+        void Oncollision()
+        {
+
+
+
+        }
+
+
+    }
+
+   
+
